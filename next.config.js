e9e6c25e42f1793a9e7016ee9d9dc4ba9c854a82
etbayes <etbayes@gmail.com>
2024-11-25 00:19:57 -0500

    fix build bugs

--- next.config.js
+++ next.config.js
@@ -1,55 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: [
       'bjuozchwgphxqrkjekpq.supabase.co',
       'm.media-amazon.com',
       'mobileimages.lowes.com'
     ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'bjuozchwgphxqrkjekpq.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'mobileimages.lowes.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-    deviceSizes: [160, 640, 750, 828, 1000, 1200],
-    imageSizes: [16, 32, 48, 64, 96, 128, 160],
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   experimental: {
-    // Required for Vercel deployment
-    esmExternals: true
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-    return config;
+    outputFileTracingRoot: process.env.VERCEL ? '/vercel/path0' : undefined,
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+      ],
+    },
   },
 };
 
